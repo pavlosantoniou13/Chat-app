f3cb3ae250cfb822c7d41a8a46573be18631a310
pavlosantoniou13 <dinant11133@gmail.com>
2023-02-23 00:43:06 +0200

    name and photo that user provides show on Homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import { signOut } from 'firebase/auth'
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 import { auth } from '../firebase'
 import porsche from '../img/porsche-logo.jpg'
 
 export default function Navbar() {
+
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <div className="navbar">
       <span className="logo">Blingo</span>
       <div className="user">
-        <img src={porsche} alt="" />
-        <span>John</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth) } >Logout</button>
       </div>
     </div>
