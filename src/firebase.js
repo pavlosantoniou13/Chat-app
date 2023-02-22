e97908e19942083adc002319927e2d8c1755a69c
pavlosantoniou13 <dinant11133@gmail.com>
2023-02-22 18:02:35 +0200

    Auth and storage added

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDgWEpAK7Co_xyPo-Sve5S6BC8SDBsDVRA",
@@ -8,8 +9,12 @@ const firebaseConfig = {
   storageBucket: "chat-c9f4f.appspot.com",
   messagingSenderId: "485694950015",
   appId: "1:485694950015:web:5723cddac4be019b198d4a"
+  
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+// authentication
 export const auth = getAuth()
+// Create a root reference
+export const storage = getStorage();
