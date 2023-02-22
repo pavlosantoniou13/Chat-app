c1f9e57ab9d1aabbc0d76facb331996121d9c427
pavlosantoniou13 <dinant11133@gmail.com>
2023-02-22 19:38:21 +0200

    App will know when au ser has logged in again using the Context API

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import Login from "./pages/Login";
 import { Register } from "./pages/Register";
 import "./style.scss"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  console.log(currentUser)
   return (
    <BrowserRouter>
       <Routes>
